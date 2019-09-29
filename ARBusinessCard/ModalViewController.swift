f2e210fb5116f6442166ac1266a3aad39f9fae35
Sun Jian <xiyuan94@gmail.com>
2019-09-29 13:07:34 +0900

    基本機能完成

---
+++ ARBusinessCard/ModalViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ModalViewController.swift
+//  ARBusinessCard
+//
+//  Created by jian sun on 2019/09/28.
+//  Copyright © 2019 jian sun. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseStorage
+import FirebaseUI
+
+class ModalViewController:UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
+ 
+    @IBOutlet weak var NameBox: UITextField!
+    @IBOutlet weak var TwitterBox: UITextField!
+    @IBOutlet weak var FacebookBox: UITextField!
+    
+    @IBOutlet weak var CardImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.NameBox.delegate = self
+        self.TwitterBox.delegate = self
+        self.FacebookBox.delegate = self
+        
+        //データ取得
+           Database.database().reference().child("person").observeSingleEvent(of: .value, with:{(snapshot) in
+               if let data = snapshot.value as? [String:AnyObject]{
+                   let name = data["Name"] as? String
+                   let Twitter = data["Twitter"] as? String
+                   let Facebook = data["Facebook"] as? String
+                   //let CardUrl = data["CardUrl"] as? String
+                
+                   self.NameBox.text = name
+                   self.TwitterBox.text = Twitter
+                   self.FacebookBox.text = Facebook
+                
+                    if let dir = FileManager.default.urls( for: .documentDirectory, in: .userDomainMask ).first {
+
+                       let filePath = dir.appendingPathComponent( "Card.png" )
+                       let path:String = filePath.path
+                        
+                        if( FileManager.default.fileExists( atPath: path ) ) {//ファイルがあったら削除
+                            let myImage = UIImage(named: path)
+                            self.CardImage.image = myImage
+
+                        }
+
+                }
+                   
+               }
+           }, withCancel: nil)
+        
+
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           textField.resignFirstResponder()
+           return true
+    }
+    
+    //　撮影が完了時した時に呼ばれる
+    func imagePickerController(_ imagePicker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        
+        // dismiss
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        if let pickedImage = info[.originalImage]
+            as? UIImage {
+            
+            CardImage.contentMode = .scaleAspectFit
+            CardImage.image = pickedImage
+            
+        }
+    }
+    
+    //アルバムから写真選択
+    @IBAction func ImgSelected(_ sender: Any) {
+        
+        let sourceType:UIImagePickerController.SourceType =
+                  UIImagePickerController.SourceType.photoLibrary
+              
+              if UIImagePickerController.isSourceTypeAvailable(
+                  UIImagePickerController.SourceType.photoLibrary){
+                  // インスタンスの作成
+                  let cameraPicker = UIImagePickerController()
+                  cameraPicker.sourceType = sourceType
+                  cameraPicker.delegate = self
+                  self.present(cameraPicker, animated: true, completion: nil)
+                  
+              }
+              else{
+                  //label.text = "error"
+                  
+              }
+              
+        
+    }
+    
+    
+    @IBAction func InsertButton(_ sender: Any) {
+        if !NameBox.text!.isEmpty && !TwitterBox.text!.isEmpty && !FacebookBox.text!.isEmpty{
+                  
+                 //Storageの参照（"Item"という名前で保存）
+                   let storageref = Storage.storage().reference(forURL: "gs://testapp-94508.appspot.com/").child("Card.PNG")
+                           
+                   //画像
+                   let image = CardImage.image
+                   //imageをNSDataに変換
+                   let data = image!.jpegData(compressionQuality: 1.0)! as NSData
+                   
+                   //Storageに保存
+                   storageref.putData(data as Data, metadata: nil) { (metadata, error) in
+                 
+                      // Fetch the download URL
+                             storageref.downloadURL{ url, error in
+                              
+                                 let downloadURL = url!.absoluteString
+                                 
+                                 //保存するデータ
+                                 let values = ["Name": self.NameBox.text,
+                                               "Twitter": self.TwitterBox.text,
+                                               "Facebook": self.FacebookBox.text,
+                                               "CardUrl":downloadURL
+                                     ] as [String : Any]
+                              
+                              Database.database().reference().child("person").updateChildValues(values as [AnyHashable : Any], withCompletionBlock: { (error, reference) in
+                                     //エラー処理
+                                     if error != nil{
+                                         print(error!)
+                                         return
+                                     }
+                                     //成功した時
+                                     
+                                 })
+                             }
+                   }
+                  
+              }
+    }
+}
---
+++ ARBusinessCard/ScanViewController.swift
@@ -0,0 +1,259 @@
+//
+//  ScanViewController.swift
+//  ARBusinessCard
+//
+//  Created by jian sun on 2019/09/28.
+//  Copyright © 2019 jian sun. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SafariServices
+import Firebase
+import FirebaseDatabase
+
+class ScanViewController: UIViewController,URLSessionDownloadDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    private var buttonNode: SCNNode!
+    
+    private let feedback = UIImpactFeedbackGenerator()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //
+        downloadImageTask()
+        
+        sceneView.delegate = self
+        
+        buttonNode = SCNScene(named: "art.scnassets/social_buttons.scn")!.rootNode.childNode(withName: "card", recursively: false)
+        let thumbnailNode = buttonNode.childNode(withName: "thumbnail", recursively: true)
+        thumbnailNode?.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "kboy_profile")
+        
+        feedback.prepare()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        
+        let configuration = ARImageTrackingConfiguration()
+        
+        let detectionImages = loadedImagesFromDirectoryContents()
+        configuration.maximumNumberOfTrackedImages = 1
+        configuration.trackingImages = detectionImages!
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //ARに表示項目定義
+        
+        guard let location = touches.first?.location(in: sceneView),
+            let result = sceneView.hitTest(location, options: nil).first else {
+                return
+        }
+        let node = result.node
+        
+        //データ取得
+        Database.database().reference().child("person").observeSingleEvent(of: .value, with:{(snapshot) in
+            if let data = snapshot.value as? [String:AnyObject]{
+                //let name = data["Name"] as? String
+                let Twitter = data["Twitter"] as? String
+                let Facebook = data["Facebook"] as? String
+                
+                if node.name == "facebook" {
+                    let safariVC = SFSafariViewController(url: URL(string: Facebook!)!)
+                    self.present(safariVC, animated: true, completion: nil)
+                } else if node.name == "twitter" {
+                    let safariVC = SFSafariViewController(url: URL(string: Twitter!)!)
+                    self.present(safariVC, animated: true, completion: nil)
+                }
+                
+            }
+        }, withCancel: nil)
+        
+
+        
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    //サーバから、名刺情報をダウンロード
+    
+    /// Downloads An Image From A Remote URL
+    func downloadImageTask(){
+        //データ取得
+        Database.database().reference().child("person").observeSingleEvent(of: .value, with:{(snapshot) in
+            if let data = snapshot.value as? [String:AnyObject]{
+                //let name = data["Name"] as? String
+                let CardUrl = data["CardUrl"] as? String
+                //1. Get The URL Of The Image
+                guard let url = URL(string: CardUrl!) else { return }
+                
+                //2. Create The Download Session
+                let downloadSession = URLSession(configuration: URLSession.shared.configuration, delegate: self, delegateQueue: nil)
+                
+                //3. Create The Download Task & Run It
+                let downloadTask = downloadSession.downloadTask(with: url)
+                downloadTask.resume()
+                
+            }
+        }, withCancel: nil)
+
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+        //1. Create The Filename
+        if let dir = FileManager.default.urls( for: .documentDirectory, in: .userDomainMask ).first {
+
+           let filePath = dir.appendingPathComponent( "Card.png" )
+           let path:String = filePath.path
+            
+            if( FileManager.default.fileExists( atPath: path ) ) {//ファイルがあったら削除
+                do {
+
+                    try FileManager.default.removeItem( atPath: path )
+                    print("画像あり")
+
+                } catch {
+                    print("画像削除失敗")
+                }
+
+            }
+
+        }
+        
+        let fileURL = getDocumentsDirectory().appendingPathComponent("Card.png")
+        //2. Copy It To The Documents Directory
+        do {
+            try FileManager.default.copyItem(at: location, to: fileURL)
+            
+            print("画像ダウンロード成功")
+            
+        } catch {
+            
+            print("画像ダウンロードエラー")
+        }
+
+        
+    }
+    
+    
+    /// Returns The Documents Directory
+    ///
+    /// - Returns: URL
+    func getDocumentsDirectory() -> URL {
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        return documentsDirectory
+        
+    }
+    
+    /// Creates A Set Of ARReferenceImages From All PNG Content In The Documents Directory
+    ///
+    /// - Returns: Set<ARReferenceImage>
+    func loadedImagesFromDirectoryContents() -> Set<ARReferenceImage>?{
+        
+        var index = 0
+        var customReferenceSet = Set<ARReferenceImage>()
+        let documentsDirectory = getDocumentsDirectory()
+        
+        do {
+            
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil, options: [])
+            
+            let filteredContents = directoryContents.filter{ $0.pathExtension == "png" }
+            
+            
+            filteredContents.forEach { (url) in
+                
+                do{
+                    
+                    //1. Create A Data Object From Our URL
+                    let imageData = try Data(contentsOf: url)
+                    guard let image = UIImage(data: imageData) else { return }
+                    
+                    //2. Convert The UIImage To A CGImage
+                    guard let cgImage = image.cgImage else { return }
+                    
+                    //3. Get The Width Of The Image
+                    //let imageWidth = CGFloat(cgImage.width)
+                    
+                    
+                    
+                    //4. Create A Custom AR Reference Image With A Unique Name
+                    let customARReferenceImage = ARReferenceImage(cgImage, orientation: CGImagePropertyOrientation.up, physicalWidth: 0.1)
+                    customARReferenceImage.name = "MyCustomARImage\(index)"
+                    
+                    //4. Insert The Reference Image Into Our Set
+                    customReferenceSet.insert(customARReferenceImage)
+                    
+                    print("画像分析成功")
+                    
+                    index += 1
+                    
+                }catch{
+                    
+                    print("画像分析エラー")
+                    
+                }
+                
+            }
+            
+        } catch {
+            
+            print("画像セットエラー")
+            
+        }
+        
+        //5. Return The Set
+        return customReferenceSet
+    }
+    
+    
+    
+    
+}
+
+extension ScanViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard anchor is ARImageAnchor else {
+            return nil
+        }
+        print("認識できました")
+        DispatchQueue.main.async {
+            self.feedback.impactOccurred()
+        }
+        buttonNode.scale = SCNVector3(0.1, 0.1, 0.1)
+        let scale1 = SCNAction.scale(to: 1.5, duration: 0.2)
+        let scale2 = SCNAction.scale(to: 1, duration: 0.1)
+        scale2.timingMode = .easeOut
+        let group = SCNAction.sequence([scale1, scale2])
+        buttonNode.runAction(group)
+        print("test")
+        
+        return buttonNode
+    }
+}
+
+
